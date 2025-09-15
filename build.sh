c0a5c66b8d0912b7e093760ed44dc0a909977891
Iliya Iliev <iliq0000@proton.me>
2025-09-15 21:15:24 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -6,41 +6,40 @@ set -e
 set -x
 
 uname -a
-for bin in make gcc clang ld ar ranlib windres; do which $bin || true; done
-clang --version
+for bin in make gcc ld ar ranlib windres; do which $bin || true; done
+gcc --version
 
 pkgname="tcl"
 [ -z "$PKG_NAME" ] || pkgname="$PKG_NAME"
+
+# build mode (forced to release in workflow, but keep for completeness)
 mode="$1"
-[ -z "$mode" ] && mode="$BUILD_MODE" # use env var if empty
-#arch="$(uname -m)"
-arch="$(clang --version | awk '/Target/{ print $2 }' | awk -F- '{print $1}')"
+[ -z "$mode" ] && mode="$BUILD_MODE"
+
+# detect arch and platform
+arch="$(gcc -dumpmachine | awk -F- '{print $1}')"
 plat="$(uname -s)"
 conf=
 case "$(echo $plat | cut -d_ -f1)" in
-    Linux)   plat=linux;  conf=unix/configure;;
-    Darwin)  plat=macos;  conf=unix/configure;;
-    MINGW64) plat=windows;conf=win/configure;;
+    Linux)    plat=linux;   conf=unix/configure ;;
+    Darwin)   plat=macos;   conf=unix/configure ;;
+    MINGW32*) plat=windows; conf=win/configure ;;
 esac
 
 [ -z "$CC" ] || echo "Using CC=$CC"
-[ -z "$CXX" ] || echo "Using CC=$CXX"
+[ -z "$CXX" ] || echo "Using CXX=$CXX"
 
 # path of this script
 scriptdir="$(cd "$(dirname "$0")" && pwd -P)"
 # output dir
 outdir="$scriptdir/$pkgname"
 mkdir -p "$outdir"
-# add debug symbols?
-DBG_ARGS=
-if [ "$mode" = "debug" ]; then
-    DBG_ARGS=--enable-symbols
-fi
 
-sh "$conf" --prefix="$outdir" --enable-static --disable-shared --enable-64bit --with-pic $DBG_ARGS
+# configure (no debug, no forced 64-bit)
+sh "$conf" --prefix="$outdir" --enable-static --disable-shared --with-pic
 
 make -j 4
-
 make install
+
 [ -z "$TAG_NAME" ] || ver="-$TAG_NAME"
-tar Jcf "${pkgname}${ver}_${plat}_${arch}_${mode}.tar.xz" "$pkgname"
+tar Jcf "${pkgname}${ver}_${plat}_${arch}_release.tar.xz" "$pkgname"
